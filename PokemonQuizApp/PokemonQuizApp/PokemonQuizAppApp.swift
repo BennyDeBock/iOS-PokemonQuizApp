b72c96fda074a1365253019b24c0683b9af68590
Benny <benny.debock@student.hogent.be>
2022-01-03 14:07:48 +0100

    Create project

---
+++ PokemonQuizApp/PokemonQuizApp/PokemonQuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonQuizAppApp.swift
+//  PokemonQuizApp
+//
+//  Created by Benny De Bock on 03/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct PokemonQuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
